e9853cd607839a4e6292dcd6bec956cc441d1d87
hidu659 <hidudkud@gmail.com>
2020-09-11 20:52:49 -0700

    committ

--- hi2.py
+++ hi2.py
@@ -1,1 +1,15 @@
-print('are you serious')
+#this program gets an items orginal price 
+#calculate its sales price with 20% discount
+
+#get the items orginal price 
+original_price = float(input('enter the items original price:'))
+
+#calculate the amount of the discount 
+discount = original_price * 0.2 
+
+# calculate the sale price 
+sale_price = original_price - discount 
+
+#Display sales price 
+#Print the sale price 
+print("the sales price is:", sale_price)
---
+++ turtle-demo.py
@@ -0,0 +1,8 @@
+import turtle 
+
+# create an isntance of the Turtle/Pen class 
+pen = turtle.Turtle()
+
+turtle.done()
+
+
